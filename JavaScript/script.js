4045454eb8bfab4d3d441d12c60bb440aa35a6e5
Hk776 <likhon.hossain20@gmail.com>
2021-02-03 17:50:54 +0600

    Assignment-4

---
+++ JavaScript/script.js
@@ -0,0 +1,104 @@
+// First class
+const firstAddBtn = document.getElementById('btn-plus');
+firstAddBtn.addEventListener( 'click', () =>{
+
+    calculationHandler( true, 'first-class-input' )
+
+} );
+
+const firstMinusBtn = document.getElementById('btn-minus');
+firstMinusBtn.addEventListener( 'click', () =>{
+    
+    calculationHandler( false, 'first-class-input' )
+
+
+} );
+
+// Economy class 
+
+const economyAddBtn = document.getElementById('btn-add-economy');
+economyAddBtn.addEventListener( 'click', () => {
+
+    calculationHandler( true, 'economy-class-input' )
+
+} );
+const economyMinusBtn = document.getElementById('btn-minus-economy');
+economyMinusBtn.addEventListener( 'click', () => {
+
+    calculationHandler( false, 'economy-class-input' )
+
+} );
+
+
+// Total
+document.getElementById('totalAmount').value = totalAmount;
+
+function  calculationHandler( increaseOrNot, inputId ){
+
+    const quantityInput = parseInt(document.getElementById(inputId).value);
+
+    if( increaseOrNot === true ){
+        const quantityResult = quantityInput + 1;
+        document.getElementById( inputId ).value = quantityResult;
+
+        const subTotal = parseFloat(document.getElementById('sub-total').innerText); 
+       
+        if( inputId === 'economy-class-input'){
+
+            const subTotalAmount = subTotal + 100 ;
+            document.getElementById('sub-total').innerText = subTotalAmount;
+
+        }
+        if( inputId === 'first-class-input'){
+
+            const subTotalAmount = subTotal + 150 ;
+            document.getElementById('sub-total').innerText = subTotalAmount;
+
+        }
+
+        const totalVat = vatCalculator(true);
+        document.getElementById('vat').innerText = totalVat;
+
+    }
+    if ( increaseOrNot === false && quantityInput > 0 ) {
+        
+        const quantityResult = quantityInput - 1;
+        document.getElementById( inputId ).value = quantityResult;
+
+        const subTotal = parseFloat(document.getElementById('sub-total').innerText); 
+
+        if( inputId === 'economy-class-input'){
+   
+            const subTotalAmount = subTotal - 100 ;
+            document.getElementById('sub-total').innerText = subTotalAmount;
+
+        }
+        if( inputId === 'first-class-input'){
+
+            const subTotalAmount = subTotal - 150 ;
+            document.getElementById('sub-total').innerText = subTotalAmount;
+
+        }
+
+        const totalVat = vatCalculator(true);
+        document.getElementById('vat').innerText = totalVat;
+
+    }
+    
+}
+
+// Function area 
+
+function vatCalculator( vatIncrease ){
+    const subTotal = parseFloat(document.getElementById('sub-total').innerText); 
+    const totalVat = (subTotal * 10 ) / 100;
+
+    if(vatIncrease === true ){
+        return totalVat;
+    }
+    
+}
+
+function totalAmount( subtotal, vat ){
+    const totalResult = subtotal + vat;
+}
